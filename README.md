578afdd79d7b872ed2946864ee552078555c7f06
Tylor Shearn <tylorshearn95@gmail.com>
2021-01-17 21:32:36 -0500

    MakeChange complete

--- README.md
+++ README.md
@@ -2,15 +2,30 @@
 
 ### Description
 
-This is a description of the Project
+ Program is intended to output the highest amount of exact change for a purchase while also informing the individual how many pieces of each currency is being provided.
 
 ### Topics and Technologies Used
 
-* Conditionals (if/else)
-* Looping (for loops)
+* Conditionals (if statements)
+* Math.ceil
+* Math.round (%)
+* Calling methods from main
 
 ### How to Run
 
-Run the application
+ Run the application.
+
+ You will be prompted to enter the price of your item.
+
+ You will be prompted to enter the amount you are giving the cashier.
+ If you enter an amount that is less than the price of the item you will be prompted to "PLEASE TRY AGAIN...". At this point you are able to enter another amount.
+
+ If you enter an amount that is equal to the price of the item you will be presented with a prompt stating "TENDERED AMOUNT EXACT NO CHANGE PROVIDED".
+
+ You will be required to run the program again if you enter the exact amount.
+
+ If you enter an amount that is greater than the price of your item, your correct change will be displayed.
 
 ### Lessons Learned
+
+ I learned that I need to create a road map before jumping into creating the program. I had to restructure my entire project because I did exactly that.
--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -1,10 +1,124 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("                ()$$ $$()");
+		System.out.println("                ()$$ $$()");
+		System.out.println("             ()$$$$$$$$$$$$$$()");
+		System.out.println("            ()$$$$$$$$$$$$$$$$()");
+		System.out.println("           ()$$$$ $$ $$ $$$$$$()");
+		System.out.println("          ()$$$$$ $$ $$ $$$$$$()");
+		System.out.println("          ()$$$$$ $$ $$ $$$$$()");
+		System.out.println("           ()$$$$ $$ $$ $$$()");
+		System.out.println("            ()$$$ $$ $$()");
+		System.out.println("             ()$$$$$ $$()");
+		System.out.println("              ()$$$$$$$()");
+		System.out.println("                ()$$$$$$$()");
+		System.out.println("                ()$$ $$$$$()");
+		System.out.println("                ()$$ $$$$$$$()");
+		System.out.println("            ()$$$ $$ $$ $$$$$()");
+		System.out.println("          ()$$$$$ $$ $$ $$$$$()");
+		System.out.println("         ()$$$$$$ $$ $$ $$$$$()");
+		System.out.println("         ()$$$$$$ $$ $$ $$$$()");
+		System.out.println("         ()$$$$$$$$$$$$$$$$()");
+		System.out.println("          ()$$$$$$$$$$$$$$()");
+		System.out.println("                ()$$ $$()");
+		System.out.println("                ()$$ $$()");
+
+		System.out.println("         _____________________ ");
+		System.out.println("        |       WELCOME       |");
+		System.out.println("        |          TO         |");
+		System.out.println("        |    CASH REGISTER    |");
+		System.out.println("        |         $$$$        |");
+		System.out.println("        |   PLEASE ENTER THE  |");
+		System.out.println("        |    PRICE OF YOUR    |");
+		System.out.println("        |         ITEM: ...   |");
+		System.out.println("        |_____________________|");
+		
+
+		Scanner kb = new Scanner(System.in);
+
+		double itemPrice = kb.nextDouble();
+
+		System.out.println("         _____________________");
+		System.out.println("        |    PLEASE ENTER     |");
+		System.out.println("        |       AMOUNT        |");
+		System.out.println("        |      TENDERED: ...  |");
+		System.out.println("        |_____________________|");
+
+		double tender = kb.nextDouble();
+		
+        if (tender == itemPrice) {
+        System.out.println("         _____________________");
+        System.out.println("        |   TENDERED AMOUNT   |");
+        System.out.println("        |        EXACT        |");
+        System.out.println("        |      NO CHANGE      |");
+        System.out.println("        |      PROVIDED       |");
+        System.out.println("        |_____________________| ");
+        	
+        }
+		
+		if (tender < itemPrice) {
+		System.out.println("         _____________________");
+		System.out.println("        |   TENDERED AMOUNT   |");
+		System.out.println("        |IS NOT SUFFICIENT FOR|");
+		System.out.println("        |      ITEM PRICE     |");
+		System.out.println("        |  PLEASE TRY AGAIN...|");
+		System.out.println("        |_____________________| ");
+		tender = kb.nextDouble();	
+		if (tender == itemPrice) {
+		System.out.println("         _____________________");
+		System.out.println("        |   TENDERED AMOUNT   |");
+		System.out.println("        |        EXACT        |");
+		System.out.println("        |      NO CHANGE      |");
+		System.out.println("        |      PROVIDED       |");
+		System.out.println("        |_____________________| ");
+			
+		}
+		}
+		
+		makeChange(itemPrice, tender);
 	}
 
+	public static int makeChange(double itemPrice, double tender) {
+
+		double result = tender - itemPrice;
+
+		if (tender > itemPrice) {
+
+		int change = (int) (Math.ceil(result * 100));
+		int singles = (int) change / 100;
+		change = change % 100;
+		int quarters = Math.round((int) change / 25);
+		change = change % 25;
+		int dimes = Math.round((int) change / 10);
+		change = change % 10;
+		int nickels = Math.round((int) change / 5);
+		change = change % 5;
+		int pennies = Math.round((int) change / 1);
+		int fives = (int) singles / 5;
+		singles = singles % 5;
+		int tens = (int) fives / 2;
+		fives = fives % 2;
+		int twenties = (int) tens / 2;
+		tens = tens % 2;
+
+		System.out.println("Your change is...");
+		System.out.println("Twenties: " + twenties);
+		System.out.println("Tens: " + tens);
+		System.out.println("Fives: " + fives);
+		System.out.println("Singles: " + singles);
+		System.out.println("Quarters: " + quarters);
+		System.out.println("Dimes: " + dimes);
+		System.out.println("Nickels: " + nickels);
+		System.out.println("Pennies: " + pennies);
+		}
+		
+		return (int) result;
+	}
 }
+
